4b24fb2b1f34df95d00cc492fdc88381fe6235ea
Bening Ranum <8705838+rbennum@users.noreply.github.com>
2025-01-06 11:00:28 +0700

    feat: remove gorm dependendy on department entity

--- entity/entity.department.go
+++ entity/entity.department.go
@@ -1,8 +1,8 @@
 package entity
 
 type Department struct {
-	Id        string `json:"department_id" gorm:"primaryKey; not null;"`
-	Name      string `json:"department_name" gorm:"unique; not null; size:33"`
-	UpdatedAt int64  `json:"updated_at" gorm:"autoUpdateTime;not null"`
-	CreatedAt int64  `json:"created_at" gorm:"autoCreateTime;not null"`
+	Id        string `json:"department_id"`
+	Name      string `json:"department_name"`
+	UpdatedAt int64  `json:"updated_at"`
+	CreatedAt int64  `json:"created_at"`
 }
