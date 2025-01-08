b0df0b3f06e89f1610eee0d5b6763d37b4db7cc2
Bening Ranum <8705838+rbennum@users.noreply.github.com>
2025-01-08 07:23:46 +0700

    feat: pull previous work

--- server/route.go
+++ server/route.go
@@ -5,11 +5,14 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/levensspel/go-gin-template/di"
 	authHandler "github.com/levensspel/go-gin-template/handler/auth"
+	departmentHandler "github.com/levensspel/go-gin-template/handler/department"
 	fileHandler "github.com/levensspel/go-gin-template/handler/file"
 	userHandler "github.com/levensspel/go-gin-template/handler/user"
 	"github.com/levensspel/go-gin-template/logger"
 	"github.com/levensspel/go-gin-template/middleware"
+	departmentRepository "github.com/levensspel/go-gin-template/repository/department"
 	fileRepository "github.com/levensspel/go-gin-template/repository/file"
+	departmentService "github.com/levensspel/go-gin-template/service/department"
 	fileService "github.com/levensspel/go-gin-template/service/file"
 	"github.com/samber/do/v2"
 
@@ -27,13 +30,16 @@ func NewRouter(r *gin.Engine, db *pgxpool.Pool) {
 	// }
 
 	fileRepo := fileRepository.NewFileRepository(db)
+	departmentRepo := departmentRepository.New(db)
 
 	fileService := fileService.NewFileService(fileRepo, logger)
+	departmentService := departmentService.New(departmentRepo, logger)
 
 	userHandler := do.MustInvoke[userHandler.UserHandler](di.Injector)
 	authHandler := do.MustInvoke[authHandler.AuthorizationHandler](di.Injector)
 
 	fileHandler := fileHandler.NewHandler(fileService, logger)
+	deptHandler := departmentHandler.New(departmentService, logger)
 
 	swaggerRoute := r.Group("/")
 	{
@@ -58,6 +64,13 @@ func NewRouter(r *gin.Engine, db *pgxpool.Pool) {
 			user.GET("", middleware.Authorization, userHandler.GetProfile)
 			user.PUT("", middleware.Authorization, userHandler.Update)
 			user.DELETE("", middleware.Authorization, userHandler.Delete)
+		}
+		department := controllers.Group("/department")
+		{
+			department.POST("", middleware.Authorization, deptHandler.Create)
+			department.GET("", middleware.Authorization, deptHandler.GetAll)
+			department.PATCH("/:id", middleware.Authorization, deptHandler.Update)
+			department.DELETE("/:id", middleware.Authorization, deptHandler.Delete)
 		}
 		// tambah route lainnya disini
 	}
