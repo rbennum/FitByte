3f7b54939e9f17c16f760bfb642240481be9916a
prasasdi <rizky.adie7@gmail.com>
2025-01-09 00:32:34 +0700

    feat/file poc upload

--- dto/dto.file.go
+++ dto/dto.file.go
@@ -3,8 +3,7 @@ package dto
 import "mime/multipart"
 
 type FileUploadRequestPayload struct {
-	File     *multipart.FileHeader `form:"file" binding:"required"`
-	FileType string                `form:"filetype binding:"required"`
+	File *multipart.FileHeader `form:"file" binding:"required"`
 }
 
 type FileUploadRespondPayload struct {
--- handler/file/handler.file.go
+++ handler/file/handler.file.go
@@ -2,6 +2,7 @@ package fileHandler
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 	"github.com/levensspel/go-gin-template/dto"
@@ -31,7 +32,6 @@ func NewHandler(service fileService.FileService, logger logger.Logger) FileHandl
 // @Produce json
 // @Param Authorization header string true "Bearer JWT token"
 // @Param File formData file true "Body with file zip"
-// @Param FileType body string true "Body with file zip"
 // @Success 200 {object} helper.Response{data=dto.FileUploadRespondPayload} "File uploaded successfully"
 // @Success 201 {object} helper.Response{data=dto.FileUploadRespondPayload} "File created successfully"
 // @Failure 400 {object} helper.Response{errors=helper.ErrorResponse} "Bad Request"
@@ -47,16 +47,19 @@ func (h handler) Upload(ctx *gin.Context) {
 		return
 	}
 
-	// Token validation logic here (e.g., decode, validate)
-	// If invalid, return unauthorized response
-
-	input := new(dto.FileUploadRequestPayload)
-
-	if err := ctx.ShouldBindJSON(&input); err != nil {
-		h.logger.Warn(err.Error(), helper.FunctionCaller("FileHandler.Upload"), &input)
-		ctx.JSON(http.StatusBadRequest, helper.NewResponse(nil, err))
+	file, header, err := ctx.Request.FormFile("File")
+	if err != nil {
+		h.logger.Warn(err.Error(), helper.FunctionCaller("FileHandler.Upload"), header)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to handle file."})
 		return
 	}
+
+	// note filename
+	// fileExt := filepath.Ext(header.Filename)
+	// originalFileName := strings.TrimSuffix(filepath.Base(header.Filename), filepath.Ext(header.Filename))
+	// now := time.Now()
+	// filename := strings.ReplaceAll(strings.ToLower(originalFileName), " ", "-") + "-" + fmt.Sprintf("%v", now.Unix()) + fileExt
+	// note;end
 
 	// Validasi ekstensi file
 	validExtensions := map[string]bool{
@@ -65,21 +68,22 @@ func (h handler) Upload(ctx *gin.Context) {
 		"image/png":  true,
 	}
 
-	if !validExtensions[input.File.Header.Get("Content-Type")] {
-		h.logger.Warn("Invalid file type. Only jpeg, jpg, or png are allowed.", helper.FunctionCaller("FileHandler.Upload"), input)
+	if !validExtensions[header.Header.Get("Content-Type")] {
+		h.logger.Warn("Invalid file type. Only jpeg, jpg, or png are allowed.", helper.FunctionCaller("FileHandler.Upload"), filepath.Ext(header.Filename))
 		ctx.JSON(http.StatusBadRequest, gin.H{"code": 415, "error": "Invalid file type. Only jpeg, jpg, or png are allowed."})
 		return
 	}
 
 	// Validasi ukuran file (max 100 KiB)
-	if input.File.Size > 1024*100 { // 100 KiB
-		h.logger.Warn("File size exceeds 100KiB.", helper.FunctionCaller("FileHandler.Upload"), input)
+	if header.Size > 1024*100 { // 100 KiB
+		h.logger.Warn("File size exceeds 100KiB.", helper.FunctionCaller("FileHandler.Upload"), header.Size)
 		ctx.JSON(http.StatusBadRequest, gin.H{"code": 413, "error": "File size exceeds 100KiB."})
 		return
 	}
 
 	// Simpan file atau proses lebih lanjut
 	// Misalnya, simpan file ke server atau S3
+	h.service.Upload(file, header)
 
 	// return file URI kembali dari service
 	ctx.JSON(http.StatusOK, helper.NewResponse(dto.FileUploadRespondPayload{
